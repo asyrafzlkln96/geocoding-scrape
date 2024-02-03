56825ba85083cdc2e8bd1de6d413ceeb1c9a65a8
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2024-02-03 22:56:41 +0800

    add latitude longitude

--- insert_table.py
+++ insert_table.py
@@ -13,30 +13,23 @@ cursor.execute('''
         name TEXT,
         address TEXT,
         opening_hours TEXT,
-        waze_url TEXT
+        waze_url TEXT,
+        latitudes REAL,
+        longitudes REAL
     )
 ''')
 
-# def insert_data(name, address, opening_hours, waze_url):
-# 	# Insert the data into the table
-#     cursor.execute('''
-#         INSERT INTO subway_locations (name, address, opening_hours, waze_url)
-#         VALUES (?, ?, ?, ?)
-#     ''', (name, address, opening_hours, waze_url))
 
-#     conn.commit()
-#     conn.close()
-
-def insert_data(names, addresses, opening_hours_list, waze_urls):
+def insert_data(names, addresses, opening_hours_list, waze_urls, latitudes, longitudes):
 	# Check if all lists have the same length
 	if len(names) != len(addresses) or len(names) != len(opening_hours_list) or len(names) != len(waze_urls):
 		print("Error: All lists must have the same length.")
 
 	# Insert the data into the table
-	data = zip(names, addresses, opening_hours_list, waze_urls)
+	data = zip(names, addresses, opening_hours_list, waze_urls, latitudes, longitudes)
 	cursor.executemany('''
-        INSERT OR REPLACE INTO subway_locations (name, address, opening_hours, waze_url)
-        VALUES (?, ?, ?, ?)
+        INSERT OR REPLACE INTO subway_locations (name, address, opening_hours, waze_url, latitudes, longitudes)
+        VALUES (?, ?, ?, ?, ?, ?)
     ''', data)
 	conn.commit()
 	# conn.close()
