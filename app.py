f4fddcc0f26eebb2739ad0a43e393c73735f9dc7
MOHD ASYRAF BIN ZULKALNAIN <53460015+asyrafzlkln96@users.noreply.github.com>
2024-02-03 23:39:35 +0800

    added pagination

--- app.py
+++ app.py
@@ -1,39 +1,54 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import sqlite3
 
 app = Flask(__name__)
 
 @app.route('/subway_locations', methods=['GET'])
 def get_subway_locations():
-	try:
-	    # Connect to the SQLite database
-	    conn = sqlite3.connect('selenium-method/subway_locations.db')
-	    cursor = conn.cursor()
+    try:
+        # Connect to the SQLite database
+        conn = sqlite3.connect('subway_locations.db')
+        cursor = conn.cursor()
 
-	    # Retrieve data from the database
-	    cursor.execute('SELECT name, address, opening_hours, waze_url, latitudes, longitudes FROM subway_locations')
-	    data = cursor.fetchall()
+        # Pagination parameters
+        page = request.args.get('page', default=1, type=int)
+        per_page = request.args.get('per_page', default=10, type=int)
 
-	    # Close the database connection
-	    conn.close()
+        # Retrieve data from the database with pagination
+        cursor.execute('SELECT name, address, opening_hours, waze_url, latitudes, longitudes FROM subway_locations LIMIT ? OFFSET ?', (per_page, (page - 1) * per_page))
+        data = cursor.fetchall()
 
-	    # Convert data to JSON format
-	    result = []
-	    for row in data:
-	        result.append({
-	            'name': row[0],
-	            'address': row[1],
-	            'opening_hours': row[2],
-	            'waze_url': row[3],
-	            'latitudes': row[4],
-	            'longitudes': row[5]
-	        })
+        # Close the database connection
+        conn.close()
 
-	    # Return the data as JSON
-	    return jsonify(result)
-	except Exception as e:
-	# Handle exceptions and return a 500 error
-		return jsonify({'Error': str(e), 'Status Code': 500}), 500
+        geojson_features = []
+        for row in data:
+            feature = {
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [float(row[5]), float(row[4])]
+                },
+                'properties': {
+                    'name': row[0],
+                    'address': row[1],
+                    'opening_hours': row[2],
+                    'waze_url': row[3]
+                }
+            }
+            geojson_features.append(feature)
+
+        geojson = {
+            'type': 'FeatureCollection',
+            'features': geojson_features
+        }
+
+        # Return the GeoJSON data
+        return jsonify(geojson)
+    except Exception as e:
+        # Handle exceptions and return a 500 error
+        return jsonify({'Error': str(e), 'Status Code': 500}), 500
+
 
 @app.route('/map', methods=['GET'])
 def show_map():
